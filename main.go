259c43999ffef5ed52ba3c9ebf1a3601c326c8b5
Sercan Karaoglu <huseyin.karaoglu@ing.com>
2019-04-04 22:44:57 +0200

    resolves #4

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"database/sql"
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/stdlib"
 )
 
 var defaultPollIntervals = []int{
@@ -37,42 +42,25 @@ func main() {
 
 	pollIntervals := getPollIntervals()
 
-	// first check if all urls are valid
-	err := isAllProtocolsValid(os.Args[1:])
-	if err != nil {
-		log.Fatalf("%v\n", err)
-	}
-
 	// dispatch polling in order received
-	for _, v := range os.Args[1:] {
-		smallv := strings.ToLower(v)
-		if strings.HasPrefix(smallv, "http") {
-			isUp, totSecs := pollHTTP200(smallv, pollIntervals)
-			if !isUp {
-				log.Fatalf("\tNot ok. Service %s polling timedout after %d seconds.\n", smallv, totSecs)
-			} else {
-				log.Printf("\tOk. Service %s is up after about %d seconds.\n", smallv, totSecs)
-			}
-		} else if strings.HasPrefix(smallv, "postgres") {
-			useDbPing := false
-			isUp, totSecs := pollPostgres(smallv, pollIntervals, useDbPing)
-			if !isUp {
-				log.Fatalf("\tNot ok. Service %s polling timedout after %d seconds.\n", smallv, totSecs)
-			} else {
-				log.Printf("\tOk. Service %s is up after about %d seconds.\n", smallv, totSecs)
-			}
-		}
-	}
-}
+	v := os.Args[1]
+	smallv := strings.ToLower(v)
 
-func isAllProtocolsValid(args []string) error {
-	for _, v := range args {
-		smallv := strings.ToLower(v)
-		if !strings.HasPrefix(smallv, "http") && !strings.HasPrefix(smallv, "postgres") {
-			return fmt.Errorf("Unrecognized protocol for %s. Allowed list is [http, postgres]", v)
+	if strings.HasPrefix(smallv, "http") {
+		isUp, totSecs := pollHTTP200(smallv, pollIntervals)
+		if !isUp {
+			log.Fatalf("\tNot ok. Service %s polling timedout after %d seconds.\n", smallv, totSecs)
+		} else {
+			log.Printf("\tOk. Service %s is up after about %d seconds.\n", smallv, totSecs)
+		}
+	} else {
+		isUp, totSecs := pollDB(smallv, pollIntervals)
+		if !isUp {
+			log.Fatalf("\tNot ok. Service %s polling timedout after %d seconds.\n", smallv, totSecs)
+		} else {
+			log.Printf("\tOk. Service %s is up after about %d seconds.\n", smallv, totSecs)
 		}
 	}
-	return nil
 }
 
 func pollHTTP200(url string, pollIntervals []int) (bool, int) {
@@ -121,93 +109,75 @@ func httpPoll(url string) bool {
 	return isIn([]int{200}, resp.StatusCode)
 }
 
-func pollPostgres(url string, pollIntervals []int, useDbPing bool) (bool, int) {
+func getConnArgs(dbURL string) (string, string, error) {
+	u, err := url.Parse(dbURL)
 
-	pg_isready := "pg_isready"
-
-	// check if pg_isready exists
-	err := isInPath(pg_isready)
 	if err != nil {
-		log.Printf("Cannot continue, required command %s not found in path.\n", pg_isready)
-		return false, -1
+		return "", "", err
 	}
 
-	// split url into host and port
-	host, port, username, err := splitPostgresURL(url)
-	if err != nil {
-		log.Printf("Invalid postgres url. Should be in the form: postgres://host:port")
-		return false, -1
+	if u.Scheme == "postgres" {
+		return "pgx", dbURL, nil
+	} else if u.Scheme == "mariadb" || u.Scheme == "mysql" {
+		return "mysql", generateConnString(u), nil
+	} else {
+		return "", "", errors.New("unsupported db protocol")
 	}
+}
 
-	log.Printf("Checking if %s is up.\n", url)
-	isUp := false
+func generateConnString(u *url.URL) string {
+	cfg := mysql.NewConfig()
+	if u.Path != "" {
+		cfg.DBName = filepath.Base(u.Path)
+	}
+	cfg.User = u.User.Username()
+	cfg.Passwd, _ = u.User.Password()
+	cfg.Addr = u.Host
+	cfg.Net = "tcp"
+	return cfg.FormatDSN()
+}
+
+func pollDB(dbURL string, pollIntervals []int) (bool, int) {
+	log.Printf("Checking if %s is up.\n", dbURL)
 	totSecs := 0
+
+	driverName, dataSourceName, err := getConnArgs(dbURL)
+	if err != nil {
+		log.Fatal(err)
+		return false, totSecs
+	}
+	db, err := sql.Open(driverName, dataSourceName)
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+
+	if err != nil {
+		log.Fatal(err)
+		defer db.Close()
+		return false, totSecs
+	}
+
 	for i := 0; i < len(pollIntervals); i++ {
 		pollInt := pollIntervals[i]
+
 		if pollInt < 0 {
 			// infinitely keep adding the same entry
 			pollIntervals = append(pollIntervals, pollInt)
 			pollInt = -pollInt
 		}
-		log.Printf("\tNext poll after %d seconds.\n", pollInt)
 		totSecs += pollInt
-		time.Sleep(time.Second * time.Duration(pollInt))
-
-		if !useDbPing {
-			var out []byte
-			username = strings.TrimSpace(username)
-
-			exe := pg_isready
-			args := []string{
-				"-h", host,
-				"-p", port,
-			}
-
-			if len(username) > 0 {
-				args = append(args, []string{"-U", username}...)
-			}
-
-			if gVerbose {
-				log.Printf("\tGoing to execute: %s, %v\n", exe, args)
-			}
-
-			out, _ = captureOutput(exec.Command(exe, args...))
-
-			log.Print("\tpg_isready response: " + string(out))
-			if bytes.Index(out, []byte("accepting connections")) >= 0 {
-				isUp = true
-				break
-			}
-		} else {
-			if postgresDBPing(url) {
-				isUp = true
-				break
-			}
+		time.Sleep(time.Duration(pollInt) * time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(pollInt)*time.Second)
+		defer cancel()
+		log.Printf("\tchecking \n")
+		err = db.PingContext(ctx)
+		if err == nil {
+			defer db.Close()
+			return true, totSecs
 		}
+		log.Printf("\tNext poll after %d seconds.\n", pollInt)
 	}
-
-	if isUp {
-		// log.Printf("\t%s is Up\n", url)
-		return true, totSecs
-	}
-
 	// log.Printf("\t%s timeout\n", url)
 	return false, totSecs
-}
-
-// url format: fmt.Sprintf("postgres://user:password@host:port/db"
-func postgresDBPing(url string) bool {
-
-	db, err := sql.Open("pgx", url)
-	if err != nil {
-		return false
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return false
-	}
-	return true
 }
 
 func captureOutput(cmd *exec.Cmd) ([]byte, error) {
@@ -222,39 +192,6 @@ func isIn(list []int, val int) bool {
 		}
 	}
 	return false
-}
-
-func isInPath(cmd string) error {
-	_, err := exec.LookPath(cmd)
-	return err
-}
-
-// returns the host, port, username of the postgres url
-func splitPostgresURL(pgURL string) (string, string, string, error) {
-	u, err := url.Parse(pgURL)
-	if err != nil {
-		return "", "", "", err
-	}
-
-	if u.Scheme != "postgres" {
-		return "", "", "", fmt.Errorf("Expected a postgres scheme. Received: %s", u.Scheme)
-	}
-
-	var host, port string
-	h := strings.Split(u.Host, ":")
-	if len(h) >= 1 {
-		host = h[0]
-	}
-	if len(h) >= 2 {
-		port = h[1]
-	}
-
-	username := ""
-	if u.User != nil {
-		username = u.User.Username()
-	}
-
-	return host, port, username, nil
 }
 
 func getPollIntervals() []int {
--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"runtime"
+	"net/url"
+	"path/filepath"
 	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func Test_isIn(t *testing.T) {
@@ -15,130 +19,28 @@ func Test_isIn(t *testing.T) {
 	}
 }
 
-func Test_isInPath(t *testing.T) {
+func Test_ConfigMysqlFormat(t *testing.T) {
+	expected := "root:admin@tcp(localhost:3307)/airlock"
+	u, _ := url.Parse("mariadb://root:admin@localhost:3307/airlock")
 
-	if runtime.GOOS == "darwin" || runtime.GOOS == "linux" {
-		if err := isInPath("date"); err != nil {
-			t.Errorf("Expected date to be in path.")
-		}
-	}
+	cfg := mysql.NewConfig()
+	cfg.DBName = filepath.Base(u.Path)
+	cfg.User = u.User.Username()
+	cfg.Passwd, _ = u.User.Password()
+	cfg.Addr = u.Host
+	cfg.Net = "tcp"
 
-	if err := isInPath("dateabcdefgh"); err == nil {
-		t.Errorf("Expected random cmd to not be in path.")
+	found := cfg.FormatDSN()
+	if expected != found {
+		t.Errorf("expected: %s found: %s", expected, found)
 	}
 }
 
-func Test_splitPostgresURL(t *testing.T) {
-	var h, p, u, hExp, pExp, uExp string
-	var err error
-	h, p, u, err = splitPostgresURL("postgres://127.0.0.1:230")
-	if err != nil {
-		t.Errorf("Expected error to be nil. Got %v.\n", err)
-	}
-	hExp = "127.0.0.1"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = "230"
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = ""
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
-	}
-
-	h, p, u, err = splitPostgresURL("postgres://127.0.0.1:230/")
-	if err != nil {
-		t.Errorf("Expected error to be %v. Got %v.\n", nil, err)
-	}
-	hExp = "127.0.0.1"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = "230"
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = ""
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
-	}
-
-	h, p, u, err = splitPostgresURL("postgres://localhost/")
-	if err != nil {
-		t.Errorf("Expected error to be %v. Got %v.\n", nil, err)
-	}
-	hExp = "localhost"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = ""
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = ""
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
-	}
-
-	h, p, u, err = splitPostgresURL("1postgres://localhost/")
-	if err == nil {
-		t.Errorf("Expected error to be non nil. Got %v.\n", err)
-	}
-}
-
-func Test_splitPostgresURL_withUser(t *testing.T) {
-	var h, p, u, hExp, pExp, uExp string
-	var err error
-	h, p, u, err = splitPostgresURL("postgres://user1@127.0.0.1:230")
-	if err != nil {
-		t.Errorf("Expected error to be nil. Got %v.\n", err)
-	}
-	hExp = "127.0.0.1"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = "230"
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = "user1"
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
-	}
-
-	h, p, u, err = splitPostgresURL("postgres://@127.0.0.1:230")
-	if err != nil {
-		t.Errorf("Expected error to be nil. Got %v.\n", err)
-	}
-	hExp = "127.0.0.1"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = "230"
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = ""
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
-	}
-
-	h, p, u, err = splitPostgresURL("postgres://abcd.efgh.ijkl@127.0.0.1:230")
-	if err != nil {
-		t.Errorf("Expected error to be nil. Got %v.\n", err)
-	}
-	hExp = "127.0.0.1"
-	if h != hExp {
-		t.Errorf("Expected host to be %s. Got %s.\n", hExp, h)
-	}
-	pExp = "230"
-	if p != pExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", pExp, p)
-	}
-	uExp = "abcd.efgh.ijkl"
-	if u != uExp {
-		t.Errorf("Expected port to be %s. Got %s.\n", uExp, u)
+func Test_generateConnString(t *testing.T) {
+	input, _ := url.Parse("mariadb://root:admin@localhost:3307/airlock")
+	want := "root:admin@tcp(localhost:3307)/airlock"
+	got := generateConnString(input)
+	if got != want {
+		t.Errorf("generateConnString() = %v, want %v", got, want)
 	}
 }
